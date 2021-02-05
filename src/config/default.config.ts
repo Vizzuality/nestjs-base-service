4e18bf1e1d09003cb839072f945570529bdd4c5a
andrea rota <andrea.rota@vizzuality.com>
2021-02-05 20:26:15 +0000

    add DEFAULT_PAGINATION config

---
+++ src/config/default.config.ts
@@ -0,0 +1,6 @@
+import { PaginationSpecification } from '../types/fetch-specification.interface';
+
+export const DEFAULT_PAGINATION: PaginationSpecification = {
+  pageSize: 25,
+  pageNumber: 1,
+};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './base.service';
 export * from './types/fetch-specification.interface';
 export * from './decorators/pagination.decorator';
 export * from './utils/pagination.utils';
+export * from './config/default.config';
