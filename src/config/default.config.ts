23d98be4ba69e5c07202f79bfcb7f174f0540b41
andrea rota <andrea.rota@vizzuality.com>
2021-03-03 18:07:36 +0000

    allow to disable pagination

--- src/config/default.config.ts
+++ src/config/default.config.ts
@@ -3,4 +3,5 @@ import { PaginationSpecification } from '../types/fetch-specification.interface'
 export const DEFAULT_PAGINATION: PaginationSpecification = {
   pageSize: 25,
   pageNumber: 1,
+  disablePagination: false,
 };
