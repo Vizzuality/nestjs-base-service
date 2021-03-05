7495631268eb45333fb21ed98edeac0bbd0e3b49
andrea rota <andrea.rota@vizzuality.com>
2021-03-05 18:14:05 +0000

    add decorator documentation

--- src/decorators/process-fetch-specification.decorator.ts
+++ src/decorators/process-fetch-specification.decorator.ts
@@ -1,5 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/**
+ * Parameter decorator: extracts `fetchSpecification` from request object.
+ */
 export const ProcessFetchSpecification = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
