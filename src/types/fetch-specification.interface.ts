dc96605b43ec3401b42e5d7f21a8848ef97630c9
andrea rota <andrea.rota@vizzuality.com>
2021-03-23 17:13:03 +0000

    add comment

--- src/types/fetch-specification.interface.ts
+++ src/types/fetch-specification.interface.ts
@@ -15,6 +15,15 @@ export interface SortSpecification {
 }
 
 export interface FiltersSpecification {
+  /**
+   * Filters supplied via query parameters, as parsed via
+   * `FetchSpecificationMiddleware` or added later in the request lifecycle
+   * (e.g. in services).
+   *
+   * All filter values are always added on the `filter` property as an array of
+   * strings. Filters set directly by services may however have other shapes,
+   * hence the `unknown` type here.
+   */
   filter?: Record<string, unknown>;
 }
 
